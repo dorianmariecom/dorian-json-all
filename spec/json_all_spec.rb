958771cc97ad0a9897361a3f4b7cf09b3b0e446f
Dorian Marié <dorian@dorianmarie.com>
2024-08-20 20:35:29 +0200

    remove dorian-times gem and related files

--- spec/json_all_spec.rb
+++ spec/json_all_spec.rb
@@ -3,11 +3,4 @@
 require "spec_helper"
 require "tempfile"
 
-RSpec.describe "json-all" do
-  it "works" do
-    file = Tempfile.create
-    File.write(file, "[1, 2, 3]")
-
-    expect(`cat #{file.path} | json-all "puts it.last"`).to eq("3\n")
-  end
 end
